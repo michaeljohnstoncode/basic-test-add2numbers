4f7bfdffc9cea39158a3da5fb2b7de3939b139d1
Michael Johnston <92060282+michaeljohnstoncode@users.noreply.github.com>
2022-04-07 15:56:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace add2numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddTwoNumbers.Addingtwonumbers(3, 6)); 
+
+          
+        }
+    }
+}
